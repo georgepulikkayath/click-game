91aa1f395e7b26076e61b5989c8d5a1e17f67452
gk <georgepulikkayath@gmail.com>
2019-06-22 19:31:11 -0400

    Updates

---
+++ precache-manifest.d21353a05b992918591c024be16b3a50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372facf624d0b036dca5c61048ecd869",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "1cf99150514d7d68827c",
+    "url": "/click-game/static/css/main.80acd4fb.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/click-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1cf99150514d7d68827c",
+    "url": "/click-game/static/js/main.833f7ca6.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
